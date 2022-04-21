7960fa2fe6f6a84487d9013d4ebae1f9c479e77e
ireshka <ireshka@users.noreply.github.com>
2022-04-21 14:52:01 +0200

    fix(front): next.config.js

--- packages/game-frontend/next.config.js
+++ packages/game-frontend/next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: 'http://localhost:4000'
+  },
 };
 
 module.exports = withNx(nextConfig);
