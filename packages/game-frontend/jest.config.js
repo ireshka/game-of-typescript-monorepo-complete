eacd2b4e2fd39d2fb20517d62826c2fe0df86ab5
ireshka <ireshka@users.noreply.github.com>
2022-04-21 10:01:57 +0200

    chore(front): add @testing-library/jest-dom

--- packages/game-frontend/jest.config.js
+++ packages/game-frontend/jest.config.js
@@ -7,4 +7,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/game-frontend',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
